f620507183dbe741117d67a9944376888aaee845
kaifahmad <kaifahmad111@gmail.com>
2026-09-20 15:11:35 +0530

    Configured https Backend API for Client services

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const apiTarget = env.VITE_API_URL || 'http://129.121.135.236:8080'
+  const apiTarget = env.VITE_API_URL || 'https://api.pharma-connect.in'
 
   return {
     plugins: [react()],
@@ -25,4 +25,4 @@ export default defineConfig(({ mode }) => {
       },
     },
   }
-})  
+})
